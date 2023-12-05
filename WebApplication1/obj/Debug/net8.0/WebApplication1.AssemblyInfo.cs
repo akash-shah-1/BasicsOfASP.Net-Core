472eae239cb8e7e1adedc2187db378f8af0137a0
CodingLifeTime-Akash <akash.s@cisinlabs.com>
2023-12-05 18:33:25 +0530

    Add toaster for beautifull notification and make it partial

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46227e7ffcc71e4ef79cfdac8a7cd0fc31bc5ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4939f4ea08c9cc4ec982ab60dbbadd7ca8e9947e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
