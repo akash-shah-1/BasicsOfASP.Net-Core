4939f4ea08c9cc4ec982ab60dbbadd7ca8e9947e
CodingLifeTime-Akash <akash.s@cisinlabs.com>
2023-12-05 18:23:57 +0530

    add Notification of TempData as a Partial view

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da88f7133b1005f01929c59d288f24d07c6706b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46227e7ffcc71e4ef79cfdac8a7cd0fc31bc5ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
